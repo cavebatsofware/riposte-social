90e7d2eb7df0726d4fd35dbfa4cefc4a5d9c73c9
Grant DeFayette <grant@cavebatsofware.com>
2026-07-11 13:50:51 -0400

    Personal profile site add

--- sites/index.ts
+++ sites/index.ts
@@ -1,11 +1,13 @@
 import type { SiteManifest } from "./manifest";
 import { randhillwoodworks } from "./randhillwoodworks";
 import { cavebatsoftware } from "./cavebatsoftware";
+import { grant } from "./grant";
 
 /** Every deployable site, keyed by manifest name. */
 export const SITES: Record<string, SiteManifest> = {
   [randhillwoodworks.name]: randhillwoodworks,
   [cavebatsoftware.name]: cavebatsoftware,
+  [grant.name]: grant,
 };
 
 export type { SiteManifest } from "./manifest";
